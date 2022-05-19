15e1d683d377c24019e7e4b93f76d889d5fdcb4d
Paru871 <paru871871@gmail.com>
2022-05-19 13:45:10 +0900

    RSpecの導入設定とrubocopで修正

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,13 @@ module Buzzcord
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -35,7 +42,7 @@ module Buzzcord
     config.active_record.default_timezone = :local
 
     config.generators.template_engine = :slim
-    
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
