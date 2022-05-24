9e4e5edc5a03c90d3745e1659977f23e8e6af633
Paru871 <paru871871@gmail.com>
2022-05-24 14:50:53 +0900

    discordrbのスレッド対応とメソッド切り出し

--- app/models/discord_bot.rb
+++ app/models/discord_bot.rb
@@ -13,30 +13,30 @@ class DiscordBot
   def recordings
     @bot.reaction_add do |event|
       if event.server.id.to_s == ENV['DISCORD_SERVER_ID']
-        Reaction.create do |reaction|
-          reaction.channel_id = event.message.channel.id
-          reaction.message_id = event.message.id
-          reaction.user_id = event.user.id
-          reaction.emoji_name = event.emoji.name
-          reaction.emoji_id = event.emoji.id
-          reaction.reacted_at = Time.current
-          reaction.type = 'add'
-        end
+        type = 'add'
+        reaction_create(event, type)
       end
     end
 
     @bot.reaction_remove do |event|
       if event.server.id.to_s == ENV['DISCORD_SERVER_ID']
-        Reaction.create do |reaction|
-          reaction.channel_id = event.message.channel.id
-          reaction.message_id = event.message.id
-          reaction.user_id = event.user.id
-          reaction.emoji_name = event.emoji.name
-          reaction.emoji_id = event.emoji.id
-          reaction.reacted_at = Time.current
-          reaction.type = 'remove'
-        end
+        type = 'remove'
+        reaction_create(event, type)
       end
+    end
+  end
+
+  private
+
+  def reaction_create(event, type)
+    Reaction.create do |reaction|
+      reaction.channel_id = event.message.channel.id
+      reaction.message_id = event.message.id
+      reaction.user_id = event.user.id
+      reaction.emoji_name = event.emoji.name
+      reaction.emoji_id = event.emoji.id
+      reaction.reacted_at = Time.current
+      reaction.type = type
     end
   end
 end
