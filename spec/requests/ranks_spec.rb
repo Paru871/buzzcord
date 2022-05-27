5b5099611b6c5db52669052a333c1402ca183c81
Paru871 <paru871871@gmail.com>
2022-05-27 22:43:44 +0900

    rubocopによる修正

--- spec/requests/ranks_spec.rb
+++ spec/requests/ranks_spec.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Ranks", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/ranks/index"
+RSpec.describe 'Ranks', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/ranks/index'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
