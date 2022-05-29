09e1cde79ed9caed78e55db471779b2beb104d62
Paru871 <paru871871@gmail.com>
2022-05-30 00:02:37 +0900

    rubocopとslim-lintの指摘に対応

--- app/models/ranks_updater.rb
+++ app/models/ranks_updater.rb
@@ -19,40 +19,79 @@ class RanksUpdater
 
     sorted_yesterday_massages.each.with_index(1) do |message, index|
       channel = yesterday_reactions.find_by(message_id: message[0]).channel_id
-      response = Discordrb::API::Channel.resolve(ENV['DISCORD_BOT_TOKEN_BOT'], channel)
-      parsed = JSON.parse(response)
+      # response = Discordrb::API::Channel.resolve(ENV['DISCORD_BOT_TOKEN_BOT'], channel)
+      # parsed = JSON.parse(response)
 
-      if parsed.key?('thread_metadata')
-        thread_id = channel
-        thread_name = parsed['name']
-        channel_id = parsed['parent_id']
-        response = Discordrb::API::Channel.resolve(ENV['DISCORD_BOT_TOKEN_BOT'], channel_id)
-        channel_name = (JSON.parse(response))['name']
-      else
-        channel_id = channel
-        channel_name = parsed['name']
-      end
+      channel_hash = make_channel_hash(channel)
+      # if parsed.key?('thread_metadata')
+      #   thread_id = channel
+      #   thread_name = parsed['name']
+      #   channel_id = parsed['parent_id']
+      #   response = Discordrb::API::Channel.resolve(ENV['DISCORD_BOT_TOKEN_BOT'], parsed['parent_id'])
+      #   channel_name = (JSON.parse(response))['name']
+      # else
+      #   channel_id = channel
+      #   channel_name = parsed['name']
+      #   thread_id = nil
+      #   thread_name = nil
+      # end
 
-      message_information = Discordrb::API::Channel.message(ENV['DISCORD_BOT_TOKEN_BOT'], channel_id, message[0])
+      message_information = Discordrb::API::Channel.message(ENV['DISCORD_BOT_TOKEN_BOT'], channel, message[0])
       message_information_parsed = JSON.parse(message_information)
 
-      rank_record = Rank.create do |rank|
-        rank.order = index
-        rank.channel_id = channel_id
-        rank.channel_name = channel_name
-        rank.thread_id = thread_id
-        rank.thread_name = thread_name
-        rank.message_id = message[0]
-        rank.content = message_information_parsed['content']
-        rank.author_id = message_information_parsed['author']['id']
-        rank.author_name = message_information_parsed['author']['username']
-        rank.author_avatar = message_information_parsed['author']['avatar']
-        rank.author_discriminator = message_information_parsed['author']['discriminator']
-      end
+      rank_record = rank_data_creation(index, channel_hash, message, message_information_parsed)
+      # Rank.create do |rank|
+      #   rank.order = index
+      #   rank.channel_id = channel_hash[:channel_id]
+      #   rank.channel_name = channel_hash[:channel_name]
+      #   rank.thread_id = channel_hash[:thread_id]
+      #   rank.thread_name = channel_hash[:thread_name]
+      #   rank.message_id = message[0]
+      #   rank.content = message_information_parsed['content']
+      #   rank.author_id = message_information_parsed['author']['id']
+      #   rank.author_name = message_information_parsed['author']['username']
+      #   rank.author_avatar = message_information_parsed['author']['avatar']
+      #   rank.author_discriminator = message_information_parsed['author']['discriminator']
+      # end
 
       create_emojis(message_information_parsed['reactions'], yesterday_reactions, message, rank_record)
 
       create_attachments(message_information_parsed, rank_record)
+    end
+  end
+
+  def make_channel_hash(channel)
+    response = Discordrb::API::Channel.resolve(ENV['DISCORD_BOT_TOKEN_BOT'], channel)
+    parsed = JSON.parse(response)
+    h = Hash.new([])
+    if parsed.key?('thread_metadata')
+      h[:thread_id] = channel
+      h[:thread_name] = parsed['name']
+      h[:channel_id] = parsed['parent_id']
+      response = Discordrb::API::Channel.resolve(ENV['DISCORD_BOT_TOKEN_BOT'], parsed['parent_id'])
+      h[:channel_name] = (JSON.parse(response))['name']
+    else
+      h[:channel_id] = channel
+      h[:channel_name] = parsed['name']
+      h[:thread_id] = nil
+      h[:thread_name] = nil
+    end
+    h
+  end
+
+  def rank_data_creation(index, channel_hash, message, message_information_parsed)
+    Rank.create do |rank|
+      rank.order = index
+      rank.channel_id = channel_hash[:channel_id]
+      rank.channel_name = channel_hash[:channel_name]
+      rank.thread_id = channel_hash[:thread_id]
+      rank.thread_name = channel_hash[:thread_name]
+      rank.message_id = message[0]
+      rank.content = message_information_parsed['content']
+      rank.author_id = message_information_parsed['author']['id']
+      rank.author_name = message_information_parsed['author']['username']
+      rank.author_avatar = message_information_parsed['author']['avatar']
+      rank.author_discriminator = message_information_parsed['author']['discriminator']
     end
   end
 
