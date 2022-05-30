f129f2ab585db0474647b560eced42ac7a5f2339
Paru871 <paru871871@gmail.com>
2022-05-30 10:45:27 +0900

    rubocopの指摘により修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
-    uid { "MyString" }
-    name { "MyString" }
+    uid { 'MyString' }
+    name { 'MyString' }
   end
 end
