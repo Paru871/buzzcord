a048d0c0adad8355ee955b3e48d365658263dc4f
Paru871 <paru871871@gmail.com>
2022-06-23 19:26:54 +0900

    view内の処理をapplication_helper.rbに移動させた

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def make_contents_all(content)
+    contents_all = +''
+    content.each do |word|
+      if word.instance_of?(Array)
+        contents_all += "<img alt='#{word[0]}' aria-label='#{word[0]}' class='emoji' data-id='#{word[1]}' data-type='emoji' draggable='false' src='https://cdn.discordapp.com/emojis/#{word[1]}.webp?size=32&amp;quality=lossless'>"
+      else
+        contents_all += word
+      end
+    end
+    contents_all
+  end
 end
